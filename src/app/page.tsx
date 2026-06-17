e35bb4bd3de655728e54ff0904b6783c08117587
ShortwireWoW <Chadricksoup@gmail.com>
2026-06-17 08:14:26 -0400

    fix: import CLASS_COLORS_HEX in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import {
   WOW_CLASSES_SPECS,
-  generateDefaultBuild
+  generateDefaultBuild,
+  CLASS_COLORS_HEX
 } from "@/lib/trainingEngine";
 
 interface Session {
